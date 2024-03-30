2c187829a411d3afbb38e4b93f54364c2aaae037
Abhishek Masne <104388375+abhitry@users.noreply.github.com>
2024-03-30 14:09:59 +0000

    comparsion

---
+++ 01_basics/04_comparsion.js
@@ -0,0 +1,9 @@
+//=== checks for value as well as datatype 
+console.log("2">1);//differnet datatype also comparable true
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+console.log(null<=0);//true
+console.log(null==0);
+console.log(null>=0);//true
